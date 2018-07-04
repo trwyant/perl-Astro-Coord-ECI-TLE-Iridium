bd057e42a4a47606564b4ae3671a22c849dfea2d
Tom Wyant <wyant@cpan.org>
2018-07-04 11:02:32 -0400

    Version 0.099_01
    Set version to 0.099_01. Update Changes file for changes since v00.099.

--- lib/Astro/Coord/ECI/TLE/Iridium.pm
+++ lib/Astro/Coord/ECI/TLE/Iridium.pm
@@ -137,7 +137,7 @@ use warnings;
 
 use base qw{Astro::Coord::ECI::TLE};
 
-our $VERSION = '0.099';
+our $VERSION = '0.099_01';
 
 use Astro::Coord::ECI::Sun;
 use Astro::Coord::ECI::Utils qw{:all};
